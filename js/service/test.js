5ce974ca6bd242995784d6d11e6b04dddfb0163c
lixinya <lixinya@seadee.com>
2017-11-22 14:58:55 +0800

    test

---
+++ js/service/test.js
@@ -0,0 +1,16 @@
+
+require(['http://qzonestyle.gtimg.cn/qzone/qzact/common/share/share.js'], function(setShareInfo) {
+    setShareInfo({
+        title:          '父爱，在你看不到的地方',
+        summary:        '父爱如山，感觉不到只因身在此山中',
+        pic:            'http://qzonestyle.gtimg.cn/aoi/sola/20150617094556_OvfOpoRKRB.png',
+        url:            'http://qzs.qzone.qq.com//qzone/qzact/act/2015/father-day-m/index.html',
+        WXconfig:       {
+            swapTitleInWX: true,
+            appId: '',
+            timestamp:'',
+            nonceStr: '',
+            signature: ''
+        }
+    });
+});
